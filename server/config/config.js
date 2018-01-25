73b312124f810a1dd4cc1c1272352acad04e7b97
Nii Yeboah <contact@niiyeboah.com>
2018-01-25 18:01:12 +0000

    Changed config

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     db: {
-        uri: 'mongodb://admin:8o7iYHRdgb5L@ds139942.mlab.com:39942/api'
+        uri: 'mongodb://niiyeboah:3g8VhLMw2OS6@ds215208.mlab.com:15208/heroku_vrn643l1'
     },
     slydePayAPI: {
         mobileNumber: '233552483882',
